f9562fc43d943fc14a77bdc1cc1645253a842806
ramilamohit <160173109+ramilamohit@users.noreply.github.com>
2024-02-21 06:16:59 -0600

    Add files via upload
    Sample commit

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+package JaveFirstDay;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.print("Hi Rukshara, Mohit");
+    System.out.print(" And Prem");
+    System.out.println("\nI'm seeking job right now\n");
+    System.out.println("Learning" + "Java");
+	}
+
+}
